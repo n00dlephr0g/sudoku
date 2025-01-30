1aa3f0a7b730af59f1239099799442a6965cc999
n00dlephr0g <monkeyjosephlee@gmail.com>
2025-01-31 05:41:01 +1100

    merged puzzle and parcer since they do the same thing

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "puzzle.c"
+#include "parcer.c"
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
--- puzzle.c
+++ puzzle.c
@@ -1,6 +1,19 @@
-#include "parcer.c"
+#include <stdio.h>
 
-int main()
+char* get_file_name()
 {
+    char *filename[50];
+    printf("enter puzzle file");
+    scanf("%s", filename);
+    filename = malloc(3 * sizeof(filename));
+    return filename;
+}
+
+int ** read_puzzle()
+{
+    FILE *filePointer;
     return 0;
 }
+
+
+
